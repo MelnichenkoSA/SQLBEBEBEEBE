217b0bd7918d15efb6cc78a69715dd70c9ddac92
melnichenkosa <gerald_mamka_geyba@mail.ru>
2023-11-15 14:48:21 +0700

    Писок

--- SQLBEBEBEEBE/MainWindow.xaml.cs
+++ SQLBEBEBEEBE/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
 
 namespace SQLBEBEBEEBE
 {
@@ -27,6 +15,10 @@ namespace SQLBEBEBEEBE
         SqlDataAdapter adapter;
         DataTable RoleTrade;
         DataTable UserTrade;
+        DataTable PickupTrade;
+        DataTable ProductTrade;
+        DataTable OrderTrade;
+        DataTable OrderProductTrade;
 
 
         public MainWindow()
@@ -38,6 +30,11 @@ namespace SQLBEBEBEEBE
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             Role();
+            User();
+            Pickup();
+            Product();
+            Order();
+            OrderProduct();
         }
 
         public void Role()
@@ -57,6 +54,33 @@ namespace SQLBEBEBEEBE
                 adapter.InsertCommand.Parameters.Add(new SqlParameter("@RoleID", SqlDbType.Int, 50, "RoleID"));
                 adapter.InsertCommand.Parameters.Add(new SqlParameter("@RoleName", SqlDbType.NVarChar, 100, "RoleName"));
 
+                connection.Open();
+                adapter.Fill(RoleTrade);
+                RoleGrid.ItemsSource = RoleTrade.DefaultView;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+
+        public void User()
+        {
+            string sql = "SELECT * FROM [User]";
+            UserTrade = new DataTable();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(sql, connection);
+                adapter = new SqlDataAdapter(command);
+
                 adapter.InsertCommand = new SqlCommand("sp_InsertUser", connection);
                 adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
                 adapter.InsertCommand.Parameters.Add(new SqlParameter("@UserID", SqlDbType.Int, 50, "UserID"));
@@ -67,34 +91,139 @@ namespace SQLBEBEBEEBE
                 adapter.InsertCommand.Parameters.Add(new SqlParameter("@UserPassword", SqlDbType.NVarChar, 100, "UserPassword"));
                 adapter.InsertCommand.Parameters.Add(new SqlParameter("@UserRole", SqlDbType.Int, 50, "UserRole"));
 
-                /*adapter.InsertCommand = new SqlCommand("sp_InsertRole", connection);
-                adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@RoleID", SqlDbType.Int, 50, "RoleID"));
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@RoleName", SqlDbType.NVarChar, 100, "RoleName"));
+                connection.Open();
+                adapter.Fill(UserTrade);
+                UserGrid.ItemsSource = UserTrade.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+        public void Pickup()
+        {
+            string sql = "SELECT * FROM PickupPoint";
+            PickupTrade = new DataTable();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(sql, connection);
+                adapter = new SqlDataAdapter(command);
 
-                adapter.InsertCommand = new SqlCommand("sp_InsertRole", connection);
+                adapter.InsertCommand = new SqlCommand("sp_InsertPickupPoint", connection);
                 adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@RoleID", SqlDbType.Int, 50, "RoleID"));
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@RoleName", SqlDbType.NVarChar, 100, "RoleName"));
-
-                adapter.InsertCommand = new SqlCommand("sp_InsertRole", connection);
-                adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@RoleID", SqlDbType.Int, 50, "RoleID"));
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@RoleName", SqlDbType.NVarChar, 100, "RoleName"));
-
-                adapter.InsertCommand = new SqlCommand("sp_InsertRole", connection);
-                adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@RoleID", SqlDbType.Int, 50, "RoleID"));
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@RoleName", SqlDbType.NVarChar, 100, "RoleName"));*/
-
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@PointID", SqlDbType.Int, 50, "PointID"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@PointAddress", SqlDbType.NVarChar, 100, "PointAddress"));
 
                 connection.Open();
-                adapter.Fill(RoleTrade);
-                RoleGrid.ItemsSource = RoleTrade.DefaultView;
-                /*PickupGrid.ItemsSource = MelnichenkosaTrade.DefaultView;
-                ProductGrid.ItemsSource = MelnichenkosaTrade.DefaultView;
-                OrderGrid.ItemsSource = MelnichenkosaTrade.DefaultView;
-                OrderProductGrid.ItemsSource = MelnichenkosaTrade.DefaultView;*/
+                adapter.Fill(PickupTrade);
+                PickupGrid.ItemsSource = PickupTrade.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+        public void Product()
+        {
+            string sql = "SELECT * FROM Product";
+            ProductTrade = new DataTable();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(sql, connection);
+                adapter = new SqlDataAdapter(command);
+
+                adapter.InsertCommand = new SqlCommand("sp_InsertProduct", connection);
+                adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@ProductArticleNumber", SqlDbType.NVarChar, 100, "ProductArticleNumber"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@ProductName", SqlDbType.NVarChar, 100, "ProductName"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@ProductDescription", SqlDbType.NVarChar, 100, "ProductDescription"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@ProductCategory", SqlDbType.NVarChar, 100, "ProductCategory"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@ProductPhoto", SqlDbType.NVarChar, 100, "ProductPhoto"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@ProductManufacturer", SqlDbType.NVarChar, 100, "ProductManufacturer"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@ProductCost", SqlDbType.Decimal, 20, "ProductCost"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@ProductDiscountAmount", SqlDbType.Int, 100, "ProductDiscountAmount"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@ProductQuantityInStock", SqlDbType.Int, 100, "ProductQuantityInStock"));
+
+                connection.Open();
+                adapter.Fill(ProductTrade);
+                ProductGrid.ItemsSource = ProductTrade.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+        public void Order()
+        {
+            string sql = "SELECT * FROM [Order]";
+            OrderTrade = new DataTable();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(sql, connection);
+                adapter = new SqlDataAdapter(command);
+
+                adapter.InsertCommand = new SqlCommand("sp_InsertOrder", connection);
+                adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@OrderID", SqlDbType.Int, 50, "OrderID"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@OrderStatus", SqlDbType.NVarChar, 100, "OrderStatus"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@OrderDeliveryDate", SqlDbType.DateTime, 100, "OrderDeliveryDate"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@OrderPickupPoint", SqlDbType.Int, 100, "OrderPickupPoint"));
+
+                connection.Open();
+                adapter.Fill(OrderTrade);
+                OrderGrid.ItemsSource = OrderTrade.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+        public void OrderProduct()
+        {
+            string sql = "SELECT * FROM OrderProduct";
+            OrderProductTrade = new DataTable();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(sql, connection);
+                adapter = new SqlDataAdapter(command);
+
+                adapter.InsertCommand = new SqlCommand("sp_InsertOrderProduct", connection);
+                adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@OrderID", SqlDbType.Int, 50, "OrderID"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@ProductArticleNumber", SqlDbType.NVarChar, 100, "ProductArticleNumber"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@NumberProduct", SqlDbType.Int, 50, "NumberProduct"));
+
+                connection.Open();
+                adapter.Fill(OrderProductTrade);
+                OrderProductGrid.ItemsSource = OrderProductTrade.DefaultView;
             }
             catch (Exception ex)
             {

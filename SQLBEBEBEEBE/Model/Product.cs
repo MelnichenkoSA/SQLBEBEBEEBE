d1866e4fdbc7f435471e4227f97d486324c5c587
melnichenkosa <gerald_mamka_geyba@mail.ru>
2024-03-12 10:13:18 +0700

    ой бой

---
+++ SQLBEBEBEEBE/Model/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLBEBEBEEBE.Model
+{
+    internal class Product
+    {
+        public int Article;
+        public string Name;
+        public string Description;
+        public string Category;
+        public string Photo;
+        public string Manufacturer;
+        public int Cost;
+        public int DiscountAmount;
+        public int Quantity;
+
+        public Product(int article, string name, string description, string category, string photo, string manufacturer, int cost, int discountAmount, int quantity)
+        {
+            Article = article;
+            Name = name;
+            Description = description;
+            Category = category;
+            Photo = photo;
+            Manufacturer = manufacturer;
+            Cost = cost;
+            DiscountAmount = discountAmount;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ SQLBEBEBEEBE/ViewModel/MainViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using SQLBEBEBEEBE.Model;
+
+namespace SQLBEBEBEEBE.ViewModel
+{
+    internal class MainViewModel
+    {
+        public int article;
+        public string name;
+        public string description;
+        public string category;
+        public string photo;
+        public string manufacturer;
+        public int cost;
+        public int discountAmount;
+        public int quantity;
+
+        private RelayCommand addProductCommand;
+        private RelayCommand updateProductCommand;
+        private RelayCommand deleteProductCommand;
+
+        public RelayCommand AddProductCommand
+        {
+            get
+            {
+                return addProductCommand ??
+                  (addProductCommand = new RelayCommand(obj =>
+                  {
+                      Product NewProduct = new Product(article, name, description, category, photo, manufacturer, cost, discountAmount, quantity);
+
+                      AddProduct(NewProduct);
+
+                  }));
+            }
+        }
+        public string AddProduct(Product NewProduct)
+        {
+            string Add;
+            Add = $"INSERT INTO Product VALUES({NewProduct.Article},'{NewProduct.Name}','{NewProduct.Description}','{NewProduct.Category}','{NewProduct.Photo}','{NewProduct.Manufacturer}',{NewProduct.Cost},{NewProduct.DiscountAmount},{NewProduct.Quantity})";
+            return Add ;
+        }
+
+        public RelayCommand UpdateProductCommand
+        {
+            get
+            {
+                return updateProductCommand ??
+                  (updateProductCommand = new RelayCommand(obj =>
+                  {
+                      //SelectedArticle = selectedArticle;
+                      //SelectedItem = selectedItem;
+
+                      UpdateProduct();
+
+                  }));
+            }
+        }
+        public string UpdateProduct()
+        {
+            string Add;
+            Add = $"UPDATE Product SET )";
+            return Add;
+        }
+
+        public RelayCommand DeleteProductCommand
+        {
+            get
+            {
+                return deleteProductCommand ??
+                  (deleteProductCommand = new RelayCommand(obj =>
+                  {
+                      //SelectedArticle = selectedArticle;
+
+                      DeleteProduct();
+
+                  }));
+            }
+        }
+        public string DeleteProduct()
+        {
+            string Add;
+            Add = $"DELETE FROM Product WHERE Article = SelectedArticle)";
+            return Add;
+        }
+    }
+
+}
+
